4bb3aaad80fa1664793dddcff93dfceae7a5a575
Mackenzzy <mackenzzy@gmail.com>
2023-10-04 20:55:13 -0300

    Criando a função do cronometro

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,9 +1,8 @@
 import React from "react"
-import { tempoParaSegundos } from "../../../common/utils/date"
+import { tempoParaSegundos } from "../../../common/utils/time"
 import style from './Relogio.module.scss'
 
 export default function Relogio(){
-    console.log('conversao: ', tempoParaSegundos('01:01:01'))
     return(
         <React.Fragment>
             <span className={style.relogioNumero}>0</span>
