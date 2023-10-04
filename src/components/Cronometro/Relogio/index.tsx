3f3ee3ec4ac6d641c31c11dd389bf60fcce27e9e
Mackenzzy <mackenzzy@gmail.com>
2023-10-04 20:41:55 -0300

    criacao de funcoes uteis, pasta common, utils, funcao de converter tempo

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,7 +1,9 @@
 import React from "react"
+import { tempoParaSegundos } from "../../../common/utils/date"
 import style from './Relogio.module.scss'
 
 export default function Relogio(){
+    console.log('conversao: ', tempoParaSegundos('01:01:01'))
     return(
         <React.Fragment>
             <span className={style.relogioNumero}>0</span>
--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,4 +1,4 @@
-import style from '../Lista.module.scss'
+import style from './Item.module.scss'
 import {ITarefa} from '../../../types/tarefa';
 
 interface Props extends ITarefa {
@@ -14,7 +14,7 @@ export default function Item({
         id,
         selecionaTarefa
     }: Props){
-    console.log ('Item atual: ', {tarefa, tempo, selecionado, completado, id} )
+  
     return(
     <li className={`${style.item} ${selecionado ? style.itemSelecionado: '' }`} onClick={() => selecionaTarefa({
             tarefa,
