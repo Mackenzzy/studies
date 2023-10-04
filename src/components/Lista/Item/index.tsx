42af502868191732963f5fd5320b51b787c6722a
Mackenzzy <mackenzzy@gmail.com>
2023-10-03 23:58:08 -0300

    Adicionando ID as tarefas

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,7 +1,8 @@
 import style from '../Lista.module.scss'
+import {ITarefa} from '../../../types/tarefa';
 
-export default function Item({tarefa, tempo}: {tarefa: string, tempo: string}){
-
+export default function Item({tarefa, tempo, selecionado, completado, id}: ITarefa){
+    console.log ('Item atual: ', {tarefa, tempo, selecionado, completado, id} )
     return(
     <li className={style.item}>
         <h3>{tarefa}</h3>
