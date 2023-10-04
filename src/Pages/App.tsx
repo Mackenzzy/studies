5306a0f1034a2fc41b05955fd52017b6508d3c4e
Mackenzzy <mackenzzy@gmail.com>
2023-10-04 20:04:32 -0300

    Criando a o props de selecionar tarefa

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -8,10 +8,20 @@ import style from './app.module.scss';
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa){
+    setSelecionado(tarefaSelecionada);
+
+
+  }
   return (
     <div className={style.AppStyle}>
-    <Formulario setTarefas={setTarefas}/>   
-    <Lista tarefas={tarefas}/> 
+    <Formulario setTarefas={setTarefas} />   
+    <Lista 
+    tarefas={tarefas}
+    selecionaTarefa = {selecionaTarefa}
+    /> 
     <Cronometro/>
     </div>
   );
