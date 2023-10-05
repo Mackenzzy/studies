37110ae1df2c9218a69652e7bdf6f4d2ba6b9827
Mackenzzy <mackenzzy@gmail.com>
2023-10-04 21:05:29 -0300

    Adicionado o tempo da tarefa ao cronometro

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -2,7 +2,7 @@ import Botao from "../Botao"
 import Relogio from "./Relogio"
 import style from "./Cronometro.module.scss"
 import { ITarefa } from "../../types/tarefa"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { tempoParaSegundos } from "../../common/utils/time"
 
 interface Props{
@@ -11,9 +11,13 @@ interface Props{
 
 export default function Cronometro({selecionado} : Props){
     const [tempo, setTempo] = useState<number>();
-    if(selecionado?.tempo){
-        setTempo(tempoParaSegundos(selecionado.tempo));
-    }
+
+    useEffect(() => {
+        if(selecionado?.tempo){
+            setTempo(tempoParaSegundos(selecionado.tempo))
+        }
+
+    },[selecionado])
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
